0296101abebf7df07a77f02885e951444c58e0d3
Hishchak Tetiana <tanaiosered@ukr.net>
2024-03-23 20:40:06 +0200

    make build

---
+++ index.ec8a0174.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.setAttribute("src",r.href)});
+//# sourceMappingURL=index.ec8a0174.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const gallery = document.querySelector(".gallery__list");
+const imgLarge = document.getElementById("largeImg");
+gallery.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const item = event.target.closest(".list-item__link");
+    imgLarge.setAttribute("src", item.href);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
